f6940236fbc7eb00951309ff8c50f40b63a3b3ea
Jay Allen <jay@endevver.com>
2013-06-24 13:32:14 -0700

    Cleanup.  Tests still 100% passing

--- lib/MT/Logger/Log4perl.pm
+++ lib/MT/Logger/Log4perl.pm
@@ -3,28 +3,38 @@ package MT::Logger::Log4perl;
 use 5.010001;
 use strict;
 use warnings FATAL => 'all';
-# use Carp::Always;
-use Data::Printer output => 'STDOUT', colored => 1;
 use Import::Into;
+use Data::Printer output => 'STDOUT', colored => 1;
+# use Carp::Always;
+
+use version 0.77; our $VERSION = qv("v2.0.0");
 
 use Moo;
 extends 'Log::Log4perl';
 
-use version 0.77; our $VERSION = qv("v2.0.0");
-
 Log::Log4perl->wrapper_register( __PACKAGE__ );
-
-BEGIN {
-    no strict 'refs'; no warnings;
-    *{__PACKAGE__."::$_"} = ${\("Log::Log4perl::".$_)}
-        for qw( init init_once init_and_watch easy_init );
-}
 
 sub config_class()          {  'MT::Logger::Log4perl::Config'          }
 sub config_class_auto()     {  'MT::Logger::Log4perl::Config::auto'    }
 sub config_class_default()  {  'MT::Logger::Log4perl::Config::default' }
+
 # Because Log4perl is dumb
 sub import  { shift; Log::Log4perl->import::into ( scalar caller, @_ ) }
+
+around [qw( init init_once init_and_watch easy_init appender_by_name
+            appender_thresholds_adjust eradicate_appender )] => sub {
+    my $orig = shift;
+    my $self = shift;
+    my @args = @_;
+    $args[0] = 'Log::Log4perl' if  $args[0] eq __PACKAGE__;
+    # p @args;
+    $orig->($self, @args);
+};
+
+after reset => sub {
+    undef $Log::Log4perl::Config::WATCHER;
+    undef $Log::Log4perl::Config::OLD_CONFIG;
+};
 
 before get_logger => sub {
     my $self = shift;
@@ -39,64 +49,6 @@ sub _auto_initialize {
         or die "Auto-initialization failed";
 }
 
-sub reset {
-    my $self = shift;
-    undef $Log::Log4perl::Config::WATCHER;
-    undef $Log::Log4perl::Config::OLD_CONFIG;
-    $self->SUPER::reset();
-}
-
 1;
 
 __END__
-
-# sub init                    {  shift->_init_proxy( 'init',           @_)    }
-# sub init_once               {  shift->_init_proxy( 'init_once',      @_)    }
-# sub init_and_watch          {  shift->_init_proxy( 'init_and_watch', @_)    }
-# sub easy_init               {  shift->_init_proxy( 'easy_init',      @_)    }
-
-# use MT::Logger::Log4perl (or subclass) qw( :resurrect )
-# sub import {
-#     my $class    = shift;
-#     my $importer = caller;
-#     goto &Log::Log4perl::import;
-# }
-
-# get_logger() can be called in the following ways:
-#
-#   (1) Log::Log4perl::get_logger()     => ()
-#   (2) Log::Log4perl->get_logger()     => ("Log::Log4perl")
-#   (3) Log::Log4perl::get_logger($cat) => ($cat)
-#   
-#   (5) Log::Log4perl->get_logger($cat) => ("Log::Log4perl", $cat)
-#   (6)   L4pSubclass->get_logger($cat) => ("L4pSubclass", $cat)
-#
-# Note that (4) L4pSubclass->get_logger() => ("L4pSubclass")
-# is indistinguishable from (3) and therefore can't be allowed.
-# Wrapper classes always have to specify the category explicitely.
-# sub _init_proxy {
-#     my $self = shift;
-#     p(@_);
-#     my $method = shift;
-#     # objectify self
-#     # create config object
-#     # config sanity check
-#     # config init
-#     p $Log::Log4perl::Config::WATCHER;
-#     Log::Log4perl->$method(@_);
-# }
-
-# sub import {
-#     my $class = shift;
-#     my $caller = caller;
-#     say STDERR "$caller importing from Log4perl: ".(join(', ', @_) || 'NONE');
-#     $class->SUPER::import( @_ );
-# }
-
-# sub get_logger {
-#     my $self   = shift;
-#     my $caller = caller;
-#     $self->_auto_initialize() unless $self->initialized;
-#     $self->SUPER::get_logger(@_);
-# }
-
--- lib/MT/Logger/Log4perl/Config.pm
+++ lib/MT/Logger/Log4perl/Config.pm
@@ -65,24 +65,6 @@ sub BUILDARGS {
 };
 
 
-# around BUILDARGS => sub {
-#     my $orig = shift;
-#     my $class = shift;
-#  
-#     if ( @_ == 1 && ! ref $_[0] ) {
-#         return $class->$orig(ssn => $_[0]);
-#     }
-#     else {
-#         return $class->$orig(@_);
-#     }
-# };
-
-# sub BUILD {
-#     my $self = shift;
-#     if ( my $c = $self->has_config ) {
-#     }
-# }
-
 sub _new {
     my $self = shift;
     return blessed( $self ) ? $self : $self->new(@_);
@@ -124,24 +106,6 @@ sub _initializer {
         warn "$driver is being re-initialized";
         $driver->reset;
     }
-
-#     # TestsFor::MT::Logger::Log4perl::Config->test_config_file()
-#         ok 1 - new with args: ( config, /Users/jay/Sites/log4mt.conf )
-#         ok 2 - The object isa MT::Logger::Log4perl::Config
-#         ok 3 - Obj has_config: 1
-# In _initializer with Printing in line 124 of lib/MT/Logger/Log4perl/Config.pm:
-# "/Users/jay/Sites/log4mt.conf"
-# conf is a path. Sending to MT::Logger::Log4perl
-# Printing in line 21 of lib/MT/Logger/Log4perl.pm:
-# [
-#     [0] "/Users/jay/Sites/log4mt.conf",
-#     [1] "HUP"
-# ]
-# Log::Log4perl configuration looks suspicious: No loggers defined at /Users/jay/perl5/perlbrew/perls/perl-5.10.1/lib/site_perl/5.10.1/Log/Log4perl/Config.pm line 317.
-#   Log::Log4perl::Config::_init('Log::Log4perl::Config', '/Users/jay/Sites') called at /Users/jay/perl5/perlbrew/perls/perl-5.10.1/lib/site_perl/5.10.1/Log/Log4perl/Config.pm line 103
-#   eval {...} called at /Users/jay/perl5/perlbrew/perls/perl-5.10.1/lib/site_perl/5.10.1/Log/Log4perl/Config.pm line 103
-#   Log::Log4perl::Config::init_and_watch('Log::Log4perl::Config', '/Users/jay/Sites/log4mt.conf', 'HUP') called at /Users/jay/perl5/perlbrew/perls/perl-5.10.1/lib/site_perl/5.10.1/Log/Log4perl.pm line 265
-#   Log::Log4perl::init_and_watch('Log::Log4perl', '/Users/jay/Sites/log4mt.conf', 'HUP') called at lib/MT/Logger/Log4perl.pm line 21
 
     return sub {
         # say STDERR "In _initializer with ".p($conf);
@@ -188,119 +152,5 @@ sub _is_path {
         || ( ! ref($c) && try { path($c); 1 } );
     return 0;
 }
-
-
-# sub init_config {
-#     my $self = shift;
-#     my @args = @_;
-#     Carp::cluck( ( Log::Log4perl->initialized() ? "Re-i" : "I" )
-#                  . "nitializing Log::Log4perl!" );
-# 
-#     @args or @args = (
-#         $self->check_environment,
-#         $self->relative_directories,
-#         $self->default_config,
-#     );
-# 
-#     my $config = first { $self->_try_config($_) } @args;
-#     $config or die "Failed to initialize Log4perl";
-# 
-#     p $config;
-#     return $config;
-# }
-
-
-### _try_config
-#
-# This method tests a configuration passed as a hash reference with related
-# metadata.
-#
-# MORE TO COME
-#
-###
-# sub _try_config {
-#     my ($self, $args ) = @_;
-# 
-#     $args = $self->_prep_config( $args )
-#         or return;
-# 
-#     return $self->_try_l4p_init( $args );
-# }
-
-# sub _prep_config {
-#     my ( $self, $args ) = @_;
-# 
-#     my ($c, $hint, $is_env) = map { $args->{$_} } qw( config type env );
-# 
-#     # Theoretically, config could be a sub reference
-#     $c = $c->() if 'CODE' eq ref($c);
-# 
-#     # Test config value and short-circuit if we got nothing
-#     return unless $c;
-# 
-#     # If for some strange reason, hint isn't defined, try to detect it
-#     $hint //=                                               # Passthru?
-#           'HASH'   eq ref($c)                   ? 'hash'    #  OK
-#         : 'GLOB'   eq ref($c)                   ? 'fh'      #  OK
-#         : 'CODE'   eq ref($c)                   ? 'code'    #  Must eval
-#         : 'SCALAR' eq ref($c)                   ? 'string'  #  OK
-#         : ( ! ref($c) && $c =~ m/\n/ )          ? 'string'  #  OK
-#         : $self->_is_configurator($c)           ? 'object'  #  OK
-#         : $self->_is_path($c)                   ? 'path'    #  Must stringify
-#                                                 : '';       #  WE'LL SEE...
-# 
-#     if ( $hint eq 'string' ) {
-#         $c = \$c unless ref $c;
-#     }
-#     elsif ( $hint eq 'path' ) {
-#         try {
-#             my $p = path($c);
-# 
-#             # If an ENV is wrong, we warn the user. Otherwise, stay silent
-#             # since most values are speculative on our part.
-#             if ( $is_env && ! $p->exists ) {
-#                 die "$is_env path ($c) could not be found";
-#             }
-# 
-#             # Convert to file if we have a dir. is_dir also checks existence
-#             $p = $p->child('log4mt.conf') if $p->is_dir;
-# 
-#             # Now, check that we have a file.  is_file also checks existence
-#             die unless $p->is_file;
-# 
-#             $args->{path} = $p;
-#         }
-#         catch {
-#             say STDERR $_ if defined($_) and length($_);
-#             undef $c;
-#         };
-#     }
-# 
-#     return $c ? $args : ();
-# }
-
-# sub _try_l4p_init {
-#     my ($self, $args) = @_;
-# 
-#     my $c = $args->{config};
-# 
-#     try {
-#         if ( my $p = $args->{path} ) {
-#             # Now send it to init_and_watch.  This will die if not kosher.
-#             Log::Log4perl->init_and_watch( $p->stringify, 'HUP' );
-#             $c = $p->stringify;
-#         }
-#         else {
-#             Log::Log4perl->init( $c );
-#         }
-#     }
-#     catch {
-#         say STDERR $_;
-#         undef $c;
-#     };
-# 
-#     return $c;
-# }
-
 
 1;
