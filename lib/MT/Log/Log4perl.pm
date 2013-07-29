03d94eaae6d6dd0839edabb1fe05eb34ce7fece2
Jay Allen <jay@endevver.com>
2013-07-29 12:49:13 -0700

    Fixed backcompat use of MT::Log::Log4perl::new

--- lib/MT/Log/Log4perl.pm
+++ lib/MT/Log/Log4perl.pm
@@ -5,11 +5,7 @@ use parent qw( MT::Logger::Log4perl );
 require Log::Log4perl;
 Log::Log4perl->wrapper_register( __PACKAGE__ );
 
-sub new {
-    my ($proto, %params) = @_;
-    my $self = $proto->SUPER::new(%params);
-    $self->get_logger();
-}
+sub new { MT::Logger::Log4perl->get_logger() }
 
 sub trace {
     my ($self, @args) = @_;
